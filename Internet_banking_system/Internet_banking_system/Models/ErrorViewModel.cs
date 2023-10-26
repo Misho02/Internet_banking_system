e6c5e597dd33c8a6d05da36ff6ab9edcceed0f4f
Michal Ryba <m1_ryba@utb.cz>
2023-10-26 20:06:44 +0200

    Vytvoření základní struktury programu. Provázání závislostí mezi jednotlivými knihovnami tříd

---
+++ Internet_banking_system/Internet_banking_system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Internet_banking_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
